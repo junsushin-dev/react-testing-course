ef6bc6e26929d8f0188d527bb723d4e9116f5586
Junsu Shin <junsushin4546@gmail.com>
2022-02-22 09:30:38 +0900

    feat: Add component and test for ScoopOption

---
+++ sundaes-on-demand/src/pages/entry/Options.jsx
@@ -0,0 +1,31 @@
+import axios from "axios";
+import { useEffect, useState } from "react";
+import ScoopOption from "./ScoopOption";
+import { Row } from "react-bootstrap";
+
+export default function Options({ optionType }) {
+  const [items, setItems] = useState([]);
+
+  //optiontype is 'scoops' or 'toppings'
+  useEffect(() => {
+    axios
+      .get(`http://localhost:3030/${optionType}`)
+      .then((response) => setItems(response.data))
+      .catch((error) => {
+        // TODO: handle error response
+      });
+  }, [optionType]);
+
+  // TODO: replace `null` with ToppingOptions when available
+  const ItemComponent = optionType === "scoops" ? ScoopOption : null;
+
+  const optionsItems = items.map((item) => (
+    <ItemComponent
+      key={item.name}
+      name={item.name}
+      imagePath={item.imagePath}
+    />
+  ));
+
+  return <Row>{optionsItems}</Row>;
+}
